34f017d471cf00dd82beafa45ba242cb6a4d7a6d
klemckinalex <126963028+klemckinalex@users.noreply.github.com>
2025-06-05 20:00:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI on Render!"}
+# Папка со статикой (CSS, JS, картинки)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Папка с шаблонами
+templates = Jinja2Templates(directory="templates")
+
+# Корневой маршрут: отображает index.html
+@app.get("/", response_class=HTMLResponse)
+async def read_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
